97f6d4a3f9fabc9b74859a5c0d47d173e7334cbf
MyBhive <delphine.miel0318@gmail.com>
2021-03-15 16:31:34 +0300

    request.get api

---
+++ ppbot/apis/map.py
@@ -0,0 +1,36 @@
+﻿import requests
+
+
+class Map:
+    """
+    Class to:
+     - find the geocode from a place through the user input question
+     - find an address after getting the geocode
+    """
+    def __init__(self, question):
+        """ Initialize the api_key, the url, and the question input to build the API"""
+        self.api_key = "AIzaSyA6pDUb-mZVASzAclRmgzkCQolxA7wTEwM"
+        self.geocode_base_url = "https://maps.googleapis.com/maps/api/geocode/json"
+        self.question = "".join(question)
+
+    def geocode(self):
+        """ Method to find the lattitude and longitude of a place answering to the user's question criteria"""
+        param = {"address": self.question,
+                 "key": self.api_key}
+        response = requests.get(self.geocode_base_url, params=param)
+        answer_json = response.json()
+        search_geocode = answer_json['results'][0]['geometry']['location']
+        if answer_json['status'] in ['OK', 'ZERO_RESULTS']:
+            return search_geocode
+        raise Exception(answer_json['error_message'])
+
+    def get_address_from_geocode(self):
+        """ Method to find the exact address after getting the geocode"""
+        param = {"address": self.question,
+                 "key": self.api_key}
+        response = requests.get(self.geocode_base_url, params=param)
+        answer_json = response.json()
+        search_geocode = answer_json['results'][0]['formatted_address']
+        if answer_json['status'] in ['OK', 'ZERO_RESULTS']:
+            return search_geocode
+        raise Exception(answer_json['error_message'])
---
+++ ppbot/apis/wiki.py
@@ -0,0 +1,43 @@
+﻿import wikipedia
+from wikipedia import exceptions
+import warnings
+
+warnings.catch_warnings()
+warnings.simplefilter("ignore")
+
+
+class Wikipedia:
+    """
+    Class to find an wikipedia answer attached to the address found in the class Map
+    """
+    def __init__(self, address_found):
+        """ Initialize the french language for the wikipedia researches and the address coming from the class Map """
+        wikipedia.set_lang("fr")
+        self.address_found = address_found
+
+    def get_wiki_result(self):
+        """ Method to search a result on wikipedia and make an output of the 3 first sentences if they are existing"""
+        try:
+            find_result = wikipedia.search(self.address_found)
+            if not find_result:
+                return "Désolé mon petit, je sais où s'est mais je ne connais pas son histoire"
+            else:
+                wik_page_name = find_result[0]
+                description = wikipedia.summary(wik_page_name, sentences=3)
+                link_search = wikipedia.page(wik_page_name)
+                connect_link = link_search.url
+                resume = ("Tu sais que {}. Si cela t'intéresse, voici le lien pour avoir plus de détail: {}"
+                          .format(description, connect_link))
+                return resume
+        except exceptions.DisambiguationError:
+            unfind = ("Ohh un papillon!? "
+                      "Tu sais quand j'étais petit les champs étaient remplis de papillon!? "
+                      "Il était aussi joli que celui-là. "
+                      "Tu viens on va manger une pomme. "
+                      "Ah oui pardon tu disais? "
+                      "Car je crois ne pas avoir de réponse à ce sujet. "
+                      "Reposes moi une autre question mais soit plus précis s'il te plait.")
+            return unfind
+
+
+
