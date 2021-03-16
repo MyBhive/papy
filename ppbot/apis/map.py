5c8bcfc5c21ef5cb2d95890a6181632c94a06b92
MyBhive <delphine.miel0318@gmail.com>
2021-03-16 12:22:16 +0300

    protection of the geocode API key

--- ppbot/apis/map.py
+++ ppbot/apis/map.py
@@ -1,4 +1,5 @@
 ﻿import requests
+import yaml
 
 
 class Map:
@@ -9,14 +10,21 @@ class Map:
     """
     def __init__(self, question):
         """ Initialize the api_key, the url, and the question input to build the API"""
-        self.api_key = ""
         self.geocode_base_url = "https://maps.googleapis.com/maps/api/geocode/json"
         self.question = "".join(question)
 
+    def yaml_loader(self, filepath):
+        """Loads the the API key for the geocoding with a yaml file"""
+        with open(filepath, "r") as file:
+            self.api_key = yaml.load(file, Loader=yaml.FullLoader)
+            for name, value in self.api_key.items():
+                return name, value
+
     def geocode(self):
         """ Method to find the lattitude and longitude of a place answering to the user's question criteria"""
+        get_key = self.yaml_loader("apis/geokey.yaml")
         param = {"address": self.question,
-                 "key": self.api_key}
+                 "key": get_key}
         response = requests.get(self.geocode_base_url, params=param)
         answer_json = response.json()
         search_geocode = answer_json['results'][0]['geometry']['location']
@@ -26,8 +34,9 @@ class Map:
 
     def get_address_from_geocode(self):
         """ Method to find the exact address after getting the geocode"""
+        get_key = self.yaml_loader("apis/geokey.yaml")
         param = {"address": self.question,
-                 "key": self.api_key}
+                 "key": get_key}
         response = requests.get(self.geocode_base_url, params=param)
         answer_json = response.json()
         search_geocode = answer_json['results'][0]['formatted_address']
