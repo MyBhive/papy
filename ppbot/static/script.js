8387f457e22fb8d0d2e04a5e92ef027db73dd00d
MyBhive <delphine.miel0318@gmail.com>
2021-04-01 12:42:04 +0300

    got the map but the rest disappear

--- ppbot/static/script.js
+++ ppbot/static/script.js
@@ -2,7 +2,7 @@ let form = document.getElementById("user_text_form");
 let json_map = "https://maps.googleapis.com/maps/api/js?";
 let spinner = document.getElementById("spinner");
 let user_question = document.querySelector("#user_input");
-const map_key = "AIzaSyA6pDUb-mZVASzAclRmgzkCQolxA7wTEwM";
+let map_key = "AIzaSyA6pDUb-mZVASzAclRmgzkCQolxA7wTEwM";
 
 
 
@@ -29,7 +29,7 @@ form.addEventListener("submit", function (event) {
         console.log(response);
         show_user();
         show_answer(response["wiki"]);
-        initMap(response["coordinate"], response["address"], map_key);
+        initMap(response["coordinate"], response["address"], map_key); 
     })
     spinner.style.visibility="hidden";
 })
@@ -44,28 +44,31 @@ function initMap(location, address, key){
     address: adresse à marquer
     key: api clef
     */
-
     let script = document.createElement("script");
     script.src = json_map + "key=" + key + "&callback=initmaps";
     script.defer = true;
+    let chat = document.getElementById("bot");
+    chat.innerHTML = `<div id="map" style="height: 400px;"></div>`;
     console.log(location)
     window.initmaps = function() {
-        const map = new google.maps.Map(document.getElementById("map"), {
-            center: location,
-            zoom: 15,
-            mapTypeId: 'satellite',
-            mapTypeControl: true, 
-            scrollwheel: false
-        });
-        new google.maps.Marker({ 
-            position: location,
-            map,
-            title: address,
-        });
-        map.setTilt(45);
+    map = new google.maps.Map(document.getElementById("map"), {
+        center: location,
+        zoom: 15,
+        mapTypeId: 'satellite',
+        mapTypeControl: true, 
+        scrollwheel: false
+    });
+    new google.maps.Marker({ 
+        position: location,
+        map: map,
+        title: address,
+    });
     } 
     document.getElementById("map").appendChild(script);
 }
+
+
+
 
 // Fonction pour HTML
 function show_user() {
@@ -74,11 +77,6 @@ function show_user() {
     document.getElementById("bot").appendChild(insert_input_user);
     user_input.value = "";
     console.log("ok ça s'affiche")
-}
-
-function answer() {
-    let answerElt = document.createElement("div");
-    answerElt.className = "offset-lg-2 col-lg-10";
 }
 
 function show_answer(wiki_quote) {
